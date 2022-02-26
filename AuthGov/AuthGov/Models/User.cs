41e8764a811567449ca7dcd351f47fe14bd47d45
Coline Berrue <coline.berrue@gmail.com>
2022-02-26 19:50:58 +0000

    Ajout Models

--- AuthGov/AuthGov/Models/User.cs
+++ AuthGov/AuthGov/Models/User.cs
@@ -1,13 +1,20 @@
-﻿using System.Dynamic;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace AuthGov;
+namespace AuthGov.Models;
 
 public class User
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     private int ID;
+    [DataType(DataType.Password)]
     private string pwd;
+    [StringLength(50)]
     private string name;
+    [DataType(DataType.EmailAddress)]
     private string address;
     private File[] _files;
 
