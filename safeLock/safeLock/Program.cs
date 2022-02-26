ca3cccf85637a58f319ef9a882752622835896de
alix.zhang <alix.zhang@epita.fr>
2022-02-26 18:15:47 +0000

    elgamal works if give private key

--- safeLock/safeLock/Program.cs
+++ safeLock/safeLock/Program.cs
@@ -2,48 +2,45 @@
 
 namespace safeLock
 {
-    public class Class1
+    class Program
     {
-        private int PublicKey;
-        private int secretKey;
-
-        public void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("hello");
-            
+            int c1 = 0, c2 = 0;
+            int p = pow_mod(2,13, 173);
+            elgamal_en(99,p,173,2,ref c1,ref c2);
+            Console.WriteLine(elgamal_de(c1,c2,13,173,2));
         }
-
-        private int pow_mod(int a, int b, int p)
+        private static int pow_mod(int a, int pui, int mod)
         {
-            int ans = 1;
-            int tmp = a % p;
-            while (b != 0)
+            int tmp = 1;
+            for(int counter = 0; counter < pui; counter++)
             {
-                if (b % 2 != 0)
-                    ans = ans * tmp % p;
-                b = b >> 1;
-                tmp = tmp * tmp * p;
-
+                
+                tmp = tmp * a;
+                tmp %= mod;
             }
 
-            return ans % p;
+            return tmp;
         }
 
-        private void elgamal_en(int m, int pub, int p, int g, ref int c1, ref int c2)
+        private static void elgamal_en(int m, int pub, int p, int g, ref int c1, ref int c2)
         {
-            int k = 5;
-            c1 = pow_mod(c1, p - 2, p);
+            Random r = new Random();
+            int k = r.Next(p - 1);
+            c1 = pow_mod(g, k, p);
             c2 = m * pow_mod(pub, k, p) % p;
+            Console.Write(c1);
         }
 
-        private int elgamal_de(int c1, int c2, int pri, int p, int g)
+        private static int elgamal_de(int c1, int c2, int pri, int p, int g)
         {
             int tmp = pow_mod(c1, p - 2, p);
             int m = c2 * pow_mod(tmp, pri, p) % p;
             return m;
         }
         
-        private int choosePrime()
+        private static int choosePrime()
         {
             int p = 0;
             do
@@ -54,7 +51,7 @@ namespace safeLock
 
             return p;
         }
-        private bool primeNumber(long n) //test if n is a prime number, we suppose n > 1
+        private static bool primeNumber(long n) //test if n is a prime number, we suppose n > 1
         {
             if (n < 3)
                 return true;
@@ -71,7 +68,7 @@ namespace safeLock
             return true;
         }
 
-        private int gcd(int a, int b)
+        private static int gcd(int a, int b)
         {
             if (a == 0)
                 return b;
@@ -88,21 +85,21 @@ namespace safeLock
 
         }
 
-        private int abs(int n)
+        private static int abs(int n)
         {
             if (n > 0)
                 return n;
             return -1 * n;
         }
 
-        private int min(int a, int b)
+        private static int min(int a, int b)
         {
             if (a > b)
                 return b;
             return a;
         }
 
-        private bool isprimeMultiple(int n)
+        private static bool isprimeMultiple(int n)
         {
             int counter = 2;
             while (counter * counter < n)
