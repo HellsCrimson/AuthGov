0281756812e4682f212c54191ac96fee8349bba7
Coline Berrue <coline.berrue@gmail.com>
2022-02-26 15:16:32 +0000

    add both classes

---
+++ AuthGov/AuthGov/File.cs
@@ -0,0 +1,18 @@
+﻿namespace AuthGov;
+
+public class File
+{
+    private string encode;
+    private string description;
+    private string title;
+    private long id;
+
+    public File(string title, string description)
+    {
+        this.title = title;
+        this.description = description;
+        id = 0;
+        encode = "";
+    }
+
+}
---
+++ AuthGov/AuthGov/User.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthGov;
+
+public class User
+{
+    private int ID;
+    private string description;
+    private string name;
+    private string address;
+    private File[] _files;
+
+    public User(string name, string address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+}
