e219d2459f7f80675352fd88ce523f069f86f435
Coline Berrue <coline.berrue@gmail.com>
2022-02-26 15:35:41 +0000

    modif class

--- AuthGov/AuthGov/File.cs
+++ AuthGov/AuthGov/File.cs
@@ -7,12 +7,12 @@ public class File
     private string title;
     private long id;
 
-    public File(string title, string description)
+    public File(string title, string description, double key)
     {
         this.title = title;
         this.description = description;
         id = 0;
-        encode = "";
+        encode = ""; //encode = encode(key)
     }
 
 }
--- AuthGov/AuthGov/User.cs
+++ AuthGov/AuthGov/User.cs
@@ -1,16 +1,33 @@
-﻿namespace AuthGov;
+﻿using System.Dynamic;
+
+namespace AuthGov;
 
 public class User
 {
     private int ID;
-    private string description;
+    private string pwd;
     private string name;
     private string address;
     private File[] _files;
 
-    public User(string name, string address)
+    public User(string name, string address, string pwd)
     {
         this.name = name;
+        this.pwd = pwd;
         this.address = address;
     }
+
+    private int getId()
+    {
+        return ID;
+    }
+
+    private void setId(int id)
+    {
+        this.ID = id;
+    }
+    
+    
+    
+    
 }
