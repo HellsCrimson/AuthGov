78b5bd150d00cd596b3cebd5a6cb2dfcb652b76c
mael reynaud <mael.reynaud@epita.fr>
2022-02-27 05:42:43 +0000

    I want to be break free

--- smartcontract/scripts/app_functions.py
+++ smartcontract/scripts/app_functions.py
@@ -6,18 +6,17 @@ import io
 from PIL import Image
 import fitz
 
-PRIVATE_KEY = ""
-PUBLIC_KEY = ""
-CONTRACT_ADDRESS = ""
+global PRIVATE_KEY
+global PUBLIC_KEY
+global CONTRACT_ADDRESS
 
 testaccountprivatekey = (
-    "c41e8481f4b4351d8cb5591411dd7a9860bd410ad3a926cdd11061bb3dbbbde6"
+    "0xc41e8481f4b4351d8cb5591411dd7a9860bd410ad3a926cdd11061bb3dbbbde6"
 )
 testaccountpublickey = "0x5C48120FBc009D31c39a8A6e15D1F112c255E856"
 
 
 def deploy_my_person(account):
-    PUBLIC_KEY = account
     my_person = MyPerson.deploy(
         {"from": account},
     )
@@ -25,19 +24,20 @@ def deploy_my_person(account):
     return my_person
 
 
-def publish_contract(account):
+def publish_contract(account, private_key):
     contract = deploy_my_person(account)
     CONTRACT_ADDRESS = contract.address
-
-    f = open("../../AuthGov/publicKey.txt", "w")
+    PUBLIC_KEY = account.address
+    PRIVATE_KEY = private_key
+    f = open("publicKey.txt", "w")
     f.write(PUBLIC_KEY)
     f.close()
 
-    f = open("../../AuthGov/privateKey.txt", "w")
+    f = open("privateKey.txt", "w")
     f.write(PRIVATE_KEY)
     f.close()
 
-    f = open("../../AuthGov/contractAddress.txt", "w")
+    f = open("contractAddress.txt", "w")
     f.write(CONTRACT_ADDRESS)
     f.close()
 
@@ -48,7 +48,7 @@ PDF = False
 
 def add(account, file):
     if TYPE:
-        addDocument(account, file, """category""")
+        addDocument(account, file, """docname""")
     else:
         addRecDocument(account, """category""", file)
 
@@ -143,6 +143,5 @@ def whatfile(filename):  # determine if an image or pdf
 
 def main():
     private_key = testaccountprivatekey
-    PRIVATE_KEY = private_key
     account = accounts.add(private_key)
-    publish_contract(account)
+    publish_contract(account, private_key)
